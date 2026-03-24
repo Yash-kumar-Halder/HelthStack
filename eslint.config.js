973f9e95730f1afbd7e18ebdb0a7f1bbe5663d9c
Yash Kumar Halder <yashkumar667453@gmail.com>
2026-03-24 23:41:42 +0530

    feat: configure husky and eslint-config-prettier eslint-plugin-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
@@ -12,6 +14,7 @@ export default defineConfig([
             js.configs.recommended,
             reactHooks.configs.flat.recommended,
             reactRefresh.configs.vite,
+            prettier, // ✅ add THIS at the END
         ],
         languageOptions: {
             ecmaVersion: 2020,
@@ -22,8 +25,12 @@ export default defineConfig([
                 sourceType: 'module',
             },
         },
+        plugins: {
+            prettier: prettierPlugin, // ✅ add plugin
+        },
         rules: {
             'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+            'prettier/prettier': 'error', // ✅ show prettier issues as ESLint errors
         },
     },
 ]);
