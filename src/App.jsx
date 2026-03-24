d1564d0178eb04d6a231927651c16644d849dcc9
Yash Kumar Halder <yashkumar667453@gmail.com>
2026-03-24 21:15:22 +0530

    chore: setup prettier configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-
-    return <>
-        <h1 className="text-center font-black text-3xl">Hospital Management System </h1>
-    </>;
+    return (
+        <>
+            <h1 className="text-center font-black text-3xl">Hospital Management System </h1>
+        </>
+    );
 }
 
 export default App;
