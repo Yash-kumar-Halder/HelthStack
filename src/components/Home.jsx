ee3ba561f7cce9dafe193b833ce55fe92b587c21
Yash Kumar Halder <yashkumar667453@gmail.com>
2026-04-28 23:27:19 +0530

    feat: add navigation & home components & initialize shadCn UI

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import LightBg from '../assets/light-bg.jpg';
+import { Button } from './ui/button';
+import { Apple, Car, Plane, Truck } from 'lucide-react';
 
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div className="relative w-full min-h-screen bg-[url('/src/assets/light-bg.jpg')] bg-cover py-14">
+            {/* content */}
+            <div className="relative">
+                <section className="w-2/3 mx-auto pt-52 flex flex-col items-center">
+                    <h1 className="text-center font-semibold text-5xl">
+                        Seamless Hospital Management <br /> For Modern Healthcare
+                    </h1>
+                    <p className="w-3/5 text-center my-3">
+                        Manage patients, appointments, billing, and staff operations in one unified
+                        system—designed to improve efficiency, accuracy, and overall healthcare
+                        delivery.
+                    </p>
+                    <div className="flex items-center gap-5 mt-2">
+                        <Button
+                            className="px-4 shadow-[0_4px_12px_rgba(0,0,0,0.7)] hover:bg-black hover:shadow-[0_4px_8px_rgba(0,0,0,0.7)] 
+                    "
+                        >
+                            Get Started
+                        </Button>
+                        <Button
+                            variant="outline"
+                            className="shadow-[0_4px_10px_rgba(0,0,0,0.5),inset_0_1px_0px_rgba(255,255,255,0.5)] px-4 font-semibold border border-neutral-200 bg-neutral-300/60 hover:bg-neutral-400/30"
+                        >
+                            Get Started
+                        </Button>
+                    </div>
+                    <div className="w-8/9 min-h-[55vh] border border-amber-700 rounded-xl mt-20 mb-10"></div>
+                </section>
+            </div>
+
+            {/* bottom gradient overlay */}
+            <div className="absolute bottom-0 left-0 w-full h-32 bg-linear-to-b from-transparent to-white"></div>
+        </div>
+    );
 };
 
 export default Home;
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,7 +1,136 @@
 import React from 'react';
+import { Button } from './ui/button';
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+    navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import { Hospital } from 'lucide-react';
+
+const components = [
+    {
+        title: 'Alert Dialog',
+        href: '/docs/primitives/alert-dialog',
+        description:
+            'A modal dialog that interrupts the user with important content and expects a response.',
+    },
+    {
+        title: 'Hover Card',
+        href: '/docs/primitives/hover-card',
+        description: 'For sighted users to preview content available behind a link.',
+    },
+    {
+        title: 'Progress',
+        href: '/docs/primitives/progress',
+        description:
+            'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
+    },
+    {
+        title: 'Scroll-area',
+        href: '/docs/primitives/scroll-area',
+        description: 'Visually or semantically separates content.',
+    },
+    {
+        title: 'Tabs',
+        href: '/docs/primitives/tabs',
+        description:
+            'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
+    },
+    {
+        title: 'Tooltip',
+        href: '/docs/primitives/tooltip',
+        description:
+            'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
+    },
+];
 
 const Navigation = () => {
-    return <div>Navigation</div>;
+    return (
+        <div className="w-2/3 h-10 fixed z-99 left-1/2 top-1.5 -translate-x-1/2 flex items-center justify-between ">
+            <a
+                href="/"
+                className="flex items-center gap-2"
+            >
+                <Hospital size={18} />
+                <h1 className="text-base font-semibold">HelthStack</h1>
+            </a>
+            <div>
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+                            <NavigationMenuContent>
+                                <ul className="w-96">
+                                    <ListItem
+                                        href="/docs"
+                                        title="Introduction"
+                                    >
+                                        Re-usable components built with Tailwind CSS.
+                                    </ListItem>
+                                    <ListItem
+                                        href="/docs/installation"
+                                        title="Installation"
+                                    >
+                                        How to install dependencies and structure your app.
+                                    </ListItem>
+                                    <ListItem
+                                        href="/docs/primitives/typography"
+                                        title="Typography"
+                                    >
+                                        Styles for headings, paragraphs, lists...etc
+                                    </ListItem>
+                                </ul>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem className="hidden md:flex">
+                            <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+                            <NavigationMenuContent>
+                                <ul className="grid w-100 gap-2 md:w-125 md:grid-cols-2 lg:w-150">
+                                    {components.map((component) => (
+                                        <ListItem
+                                            key={component.title}
+                                            title={component.title}
+                                            href={component.href}
+                                        >
+                                            {component.description}
+                                        </ListItem>
+                                    ))}
+                                </ul>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuLink
+                                asChild
+                                className={navigationMenuTriggerStyle()}
+                            >
+                                <a href="/docs">Docs</a>
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </div>
+            <Button className="text-sm px-4 py-1.5 h-fit hover:bg-neutral-700">Login</Button>
+        </div>
+    );
 };
+
+function ListItem({ title, children, href, ...props }) {
+    return (
+        <li {...props}>
+            <NavigationMenuLink asChild>
+                <a href={href}>
+                    <div className="flex flex-col gap-1 text-sm">
+                        <div className="leading-none font-medium">{title}</div>
+                        <div className="line-clamp-2 text-muted-foreground">{children}</div>
+                    </div>
+                </a>
+            </NavigationMenuLink>
+        </li>
+    );
+}
 
 export default Navigation;
